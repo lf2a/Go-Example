96400b84990d958a757b3a550ff44cb660cf43ff
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-22 12:27:36 -0300

    nada de importante mudado

--- enum.go
+++ enum.go
@@ -6,14 +6,14 @@ type Semana uint8
 
 const (
 	domingo Semana = iota + 1
-	segunda
-	terca
-	quarta
-	quinta
-	sexta
-	sabado
+	segunda        // 2
+	terca          // 3
+	quarta         // 4
+	quinta         // 5
+	sexta          // 6
+	sabado         // 7
 )
 
-func EnumTest() {
+func enum() {
 	fmt.Println(domingo)
 }
