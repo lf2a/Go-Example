1bd51f1b8b61149c0bca240255d243756657c206
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-22 12:26:42 -0300

    colocando espaços, mudando nome de variveis e a função que lança o erro

--- error.go
+++ error.go
@@ -6,27 +6,28 @@ import (
 	"log"
 )
 
-func add(v1, v2 int32) (int32, error) {
+func soma(v1, v2 int32) (int32, error) {
 	if v1 == 0 && v2 == 0 {
 		return -1, errors.New("Os valores devem ser diferentes de zero")
 	}
+
 	return v1 + v2, nil
 }
 
-func ErrorTest() {
-	res1, err1 := add(12, 2)
-	if err1 != nil {
-		log.Panicln(err1)
-		// fmt.Println(err1)
-	} else {
-		fmt.Println(res1)
-	}
+func erro() {
+	res, err := soma(12, 2)
 
-	res, err := add(0, 0)
 	if err != nil {
 		log.Panicln(err)
-		// fmt.Println(err)
-	} else {
-		fmt.Println(res)
 	}
+
+	fmt.Println("res", res)
+
+	res2, err2 := soma(0, 0)
+
+	if err2 != nil {
+		log.Panicln(err2)
+	}
+
+	fmt.Println("res2:", res2)
 }
