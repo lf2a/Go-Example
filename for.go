eb826a042bd257d691f8aefac8404728e2480bde
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-24 22:10:41 -0300

    mais comentarios e exemplo duplicado removido

--- for.go
+++ for.go
@@ -2,19 +2,14 @@ package main
 
 import "fmt"
 
-func ForTest() {
+func main() {
+	// for padão
 	for i := 0; i < 5; i++ {
 		fmt.Printf("index: %d\n", i)
 	}
 
-	fmt.Println("")
-	sub := 5
-	for sub > 0 {
-		sub--
-		fmt.Printf("sub is %d\n", sub)
-	}
+	fmt.Println("===================================")
 
-	fmt.Println("")
 	// `while`
 	i := 0
 	for i < 5 {
@@ -22,17 +17,17 @@ func ForTest() {
 		i++
 	}
 
-	// infinity loop
+	// loop infinito
 	// for {}
 
-	// range
+	// range (foreach)
 	w := []int{1, 2, 4, 8}
-
+	// o index é o primeiro valor (i) e o valor do indice é (v)
 	for i, v := range w {
 		fmt.Printf("index: %d value: %d\n", i, v)
 	}
 
-	// `_` omitindo o valor
+	// `_` omitindo (ignorando) o valor
 	for _, v := range w {
 		fmt.Printf("valor: %d\n", v)
 	}
